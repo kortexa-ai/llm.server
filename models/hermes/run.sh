145c42ad94575145da2a21ef5edb1eba0f1f7046
Franci Penov <francip@gmail.com>
2025-04-26 23:08:33 -0700

    Updating Hermes config

--- models/hermes/run.sh
+++ models/hermes/run.sh
@@ -2,6 +2,8 @@
 source .venv/bin/activate
 python -m vllm.entrypoints.openai.api_server \
     --model adamo1139/Hermes-3-Llama-3.1-8B-FP8-Dynamic \
+    --gpu-memory-utilization 0.95 \
+    --cpu-offload-gb 0 \
     --host 0.0.0.0 \
     --port 18888 \
-    --max-model-len 48000
+    --max-model-len 80000
